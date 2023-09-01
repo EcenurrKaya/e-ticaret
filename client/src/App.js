d273180296513c4424576e495aa03dce9f375aee
Ecenur Kaya <kaya.ecenur02@gmail.com>
2023-09-01 14:01:08 +0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {createBrowserRouter,RouterProvider,Route,Link,Router,Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/" component={Home}></Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+}
+
+function Home(){
+  return <h2>About</h2>;
 }
 
 export default App;
